a1bf2a7bed3779a5745eb5220db2e37333b1243a
David Cramer <dcramer@gmail.com>
2015-04-22 13:49:59 -0700

    Ensure form reflects optional value on recipients

--- src/sentry_opsgenie/plugin.py
+++ src/sentry_opsgenie/plugin.py
@@ -20,19 +20,19 @@ class OpsGenieptionsForm(notify.NotificationConfigurationForm):
     api_key = forms.CharField(
         max_length=255,
         help_text='OpsGenie API key used for authenticating API requests',
-        required=True
+        required=True,
     )
     recipients = forms.CharField(
         max_length=255,
         help_text='The user names of individual users or groups (comma seperated)',
-        required=True
+        required=False,
     )
     alert_url = forms.CharField(
         max_length=255,
         label='OpsGenie Alert URL',
         widget=forms.TextInput(attrs={'class': 'span6', 'placeholder': 'e.g. https://api.opsgenie.com/v1/json/alert'}),
         help_text='It must be visible to the Sentry server',
-        required=True
+        required=True,
     )
 
 
